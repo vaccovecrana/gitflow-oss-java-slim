bd9873c3ad7cef5efa6dfb7566538df1a1df93d9
Jesus Zazueta <jjzazuet@gmail.com>
2025-07-13 15:07:06 -0400

    - Maven Central Portalz...

--- gs-gradle-plugin/bundle.sh
+++ gs-gradle-plugin/bundle.sh
@@ -1,12 +1,17 @@
 # brew install coreutils
-
 rm -rfv ~/.m2/repository/io/vacco/oss/gitflow
 gradle clean sign publishToMavenLocal
 cd ~/.m2/repository
 pwd
+
+tree .
 rm -fv io/vacco/oss/gitflow/io.vacco.oss.gitflow.gradle.plugin/maven-metadata-local.xml
-find . -type f -exec sh -c 'md5sum  "$1" > "$1.md5"' _ {} \;
-find . -type f -exec sh -c 'sha1sum "$1" > "$1.sha"' _ {} \;
+rm -fv io/vacco/oss/gitflow/io.vacco.oss.gitflow.gradle.plugin/1.8.0/io.vacco.oss.gitflow.gradle.plugin-1.8.0.module*
+find . -type f -not -name "*.asc" -not -name "*.md5" -not -name "*.sha1" -exec sh -c 'md5sum  "$1" | awk "{print \$1}" > "$1.md5"' _ {} \;
+find . -type f -not -name "*.asc" -not -name "*.md5" -not -name "*.sha1" -exec sh -c 'sha1sum "$1" | awk "{print \$1}" > "$1.sha1"' _ {} \;
+tree .
+rm -fv ~/Desktop/bundle.zip
 zip -r ~/Desktop/bundle.zip .
+unzip -l ~/Desktop/bundle.zip
 
 echo 'Upload to Maven Central portal... sigh...'
